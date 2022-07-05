2bc0f9c36c96cbc81e1d4e8213b85110aa1090a9
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2022-07-05 21:47:04 +0200

    Add Initial Project

---
+++ KivuShot/KivuShot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KivuShot
+//
+//  Created by Cédric Bahirwe on 05/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KivuShot/KivuShot/KivuShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  KivuShotApp.swift
+//  KivuShot
+//
+//  Created by Cédric Bahirwe on 05/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct KivuShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
